6660fe3766261c53a8148191946652e4dfee7f68
leobur <leonardo.burrone@matera.com>
2021-04-03 16:35:51 -0300

    Create balance sagas

---
+++ src/Ducks/Balance/Actions.ts
@@ -0,0 +1,16 @@
+// TODO: testar actions
+import { BalanceActionTypes, ChangeBalanceAction, ChangeBalanceErrorAction, FetchBalanceAction } from './types';
+
+export const changeBalanceAction = (payload: string): ChangeBalanceAction => ({
+  payload,
+  type: BalanceActionTypes.CHANGE_BALANCE
+});
+
+export const changeBalanceErrorAction = (payload: boolean): ChangeBalanceErrorAction => ({
+  payload,
+  type: BalanceActionTypes.CHANGE_BALANCE_ERROR
+});
+
+export const fetchBalanceAction = (): FetchBalanceAction => ({
+  type: BalanceActionTypes.FETCH_BALANCE
+});
--- src/Store/RootSagas.ts
+++ src/Store/RootSagas.ts
@@ -1,9 +1,10 @@
 import { all } from 'redux-saga/effects';
 
+import { balanceSagas } from '../Ducks/Balance/Sagas';
 import { dashboardSagas } from '../Ducks/Dashboard/Sagas';
 import { loginSagas } from '../Ducks/Login/Sagas';
 import { signUpSagas } from '../Ducks/SignUp/Sagas';
 
 export default function* rootSagas() {
-  yield all([dashboardSagas, loginSagas, signUpSagas]);
+  yield all([balanceSagas, dashboardSagas, loginSagas, signUpSagas]);
 }
