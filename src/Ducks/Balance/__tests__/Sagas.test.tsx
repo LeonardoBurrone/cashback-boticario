2f1760d2b0b6a914ff227bf74165691ae1d92afe
leobur <leonardo.burrone@matera.com>
2021-04-05 11:56:47 -0300

    Test Balance sagas

---
+++ src/Ducks/Balance/__tests__/Sagas.test.tsx
@@ -0,0 +1,37 @@
+import { fetchBalanceAction } from '../Actions';
+import { fetchBalance } from '../Sagas';
+import { BalanceActionTypes, BalanceResponse } from '../types';
+
+import { recordSaga } from '../../../Helpers/SagasTests';
+import Api from '../../../Services/Api';
+
+jest.mock('../../../Services/Api');
+
+describe('Test Balance sagas', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Test fetchBalance saga - success', async () => {
+    Api.get = jest.fn(async () => Promise.resolve<BalanceResponse>({ balance: '100' }));
+
+    const dispatched: any = await recordSaga(fetchBalance, fetchBalanceAction(), {});
+
+    expect(dispatched[0].payload).toBe('100');
+    expect(dispatched[0].type).toBe(BalanceActionTypes.CHANGE_BALANCE);
+    expect(dispatched[1].payload).toBe(false);
+    expect(dispatched[1].type).toBe(BalanceActionTypes.CHANGE_BALANCE_ERROR);
+  });
+
+  it('Test fetchBalance saga - error', async () => {
+    const error = new Error('Error message');
+    Api.get = jest.fn(async () => Promise.reject(error));
+
+    const dispatched: any = await recordSaga(fetchBalance, fetchBalanceAction(), {});
+
+    expect(dispatched[0].payload).toBe('');
+    expect(dispatched[0].type).toBe(BalanceActionTypes.CHANGE_BALANCE);
+    expect(dispatched[1].payload).toBe(true);
+    expect(dispatched[1].type).toBe(BalanceActionTypes.CHANGE_BALANCE_ERROR);
+  });
+});
